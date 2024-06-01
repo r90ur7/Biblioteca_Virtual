d6b6cdcc0e544c342265c669fc6f8d2231fbed98
Rallenson <Rallenson90@gmail.com>
2024-06-01 18:53:40 -0300

    chore: Add initial project files and configurations

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\LibraryController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+// Rota Autenticação
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class,'index'])->name('home');
+
+
+// Rotas Usuário
+Route::get('/users', [UserController::class,'index'])->name('users.index');
+Route::get('/users/create', [UserController::class,'create'])->name('users.create');
+Route::post('/users', [UserController::class,'store'])->name('users.store');
+Route::post('/users/{user}', [UserController::class,'show'])->name('users.show');
+Route::post('/users/{user}/edit', [UserController::class,'edit'])->name('users.edit');
+Route::put('/users/{user}', [UserController::class,'update'])->name('users.updtate');
+Route::delete('', [UserController::class,'destroy'])->name('users.destroy');
+
+// Rotas Livros
+Route::get('/library', [LibraryController::class,'index'])->name('library.index');
+Route::get('/library/create', [LibraryController::class,'create'])->name('library.create');
+Route::post('/library', [LibraryController::class,'store'])->name('library.store');
+Route::post('/library/{book}', [LibraryController::class,'show'])->name('library.show');
+Route::post('/library/{book}/edit', [LibraryController::class,'edit'])->name('library.edit');
+Route::put('/library/{book}', [LibraryController::class,'update'])->name('library.update');
+Route::delete('/library/{book}', [LibraryController::class,'destroy'])->name('library.destroy');
