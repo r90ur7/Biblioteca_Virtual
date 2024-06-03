598cc3e55835f06710ea7e66c5aa52cc187673b7
Rallenson <Rallenson90@gmail.com>
2024-06-02 21:37:25 -0300

    feat: Update user routes to use resourceful routing
    This commit updates the user routes in the `web.php` file to use resourceful routing. The previous individual route declarations for index, create, store, show, edit, update, and destroy are commented out, and a single `Route::resource` declaration is added to handle all the CRUD operations for the `UserController` class. This change simplifies the code and follows the convention for RESTful APIs.

--- routes/web.php
+++ routes/web.php
@@ -23,13 +23,14 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('/', [HomeController::class,'index'])->name('home');
 
 // Rotas Usuário
-Route::get('/users', [UserController::class,'index'])->name('users.index');
-Route::get('/users/create', [UserController::class,'create'])->name('users.create');
-Route::post('/users', [UserController::class,'store'])->name('users.store');
-Route::post('/users/{user}', [UserController::class,'show'])->name('users.show');
-Route::post('/users/{user}/edit', [UserController::class,'edit'])->name('users.edit');
-Route::put('/users/{user}', [UserController::class,'update'])->name('users.updtate');
-Route::delete('', [UserController::class,'destroy'])->name('users.destroy');
+// Route::get('/users', [UserController::class,'index'])->name('users.index');
+// Route::get('/users/create', [UserController::class,'create'])->name('users.create');
+// Route::post('/users', [UserController::class,'store'])->name('users.store');
+// Route::post('/users/{user}', [UserController::class,'show'])->name('users.show');
+// Route::post('/users/{user}/edit', [UserController::class,'edit'])->name('users.edit');
+// Route::put('/users/{user}', [UserController::class,'update'])->name('users.updtate');
+// Route::delete('', [UserController::class,'destroy'])->name('users.destroy');
+Route::resource('users',UserController::class);
 
 // Rotas Livros
 Route::get('/library', [LibraryController::class,'index'])->name('library.index');
